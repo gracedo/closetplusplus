044c79b921e96bde73cdfccf9fed6ac1f445e0d3
Grace Do <xgrace@gmail.com>
2014-03-19 11:21:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Closet::Application.config.secret_key_base = '63baa2ec7364cee26015f8afdc896b9fe44f8cb5fa264a644c2191200f1a84619449497a5be057d8ec6133811c34eea5e272e4bfb64652988886692a7f38adad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Closet::Application.config.session_store :cookie_store, key: '_closet++_session'
