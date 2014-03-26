95e76228d38988462beb06472729e249cd6a2f56
Grace Do <xgrace@gmail.com>
2014-03-26 14:05:55 -0700

    upper body measurement sliders

--- app/assets/javascripts/views/measurements/measurements_show.js
+++ app/assets/javascripts/views/measurements/measurements_show.js
@@ -22,7 +22,12 @@ Closet.Views.MeasurementsShow = Backbone.View.extend({
     var renderedContent = this.template({
       user: this.user,
       measurements: this.model,
-      action: action
+      action: action,
+      pos: "left",
+      WEIGHT: ["weight"],
+      UPPER_BODY: ["chest", "belly", "neck", "shoulder", "torso", "arm", "sleeve"],
+      LOWER_BODY: ["waist", "inseam", "thigh", "hip"],
+      SHOE_SIZE: ["shoe_size"]
     });
     
     this.$el.html(renderedContent);
@@ -37,7 +42,7 @@ Closet.Views.MeasurementsShow = Backbone.View.extend({
     debugger
     if(!newMeasures.isValid()) {
       console.log("measurements failed to be created");
-      $(".alert").append("Please do not leave any fields blank.");
+      $(".alert").html("Invalid Attributes!");
       $(".alert").removeClass("hidden");
     } else {
       this.model.save($formData, {
@@ -82,29 +87,45 @@ Closet.Views.MeasurementsShow = Backbone.View.extend({
   
   initSliders: function() {
     this.heightSlider();
-    this.generateSlider("weight", 100, 300, 5, "100 LBS");
+    this.generateSlider("weight", 100, 300, 5, " lbs", "100 lbs");
+    // Upper body measurements
+    this.generateSlider("chest", 32, 55, 1, "''", "32''");
+    this.generateSlider("belly", 28, 55, 1, "''", "28''");
+    this.generateSlider("neck", 14, 21, 0.5, "''", "14''");
+    this.generateSlider("shoulder", 15, 30, 1, "''", "15''");
+    this.generateSlider("torso", 24, 35, 1, "''", "24''");
+    this.generateSlider("arm", 10, 20, 1, "''", "10''");
+    this.generateSlider("sleeve", 30, 40, 1, "''", "30''");
+    // Lower body measurements
+    this.generateSlider("waist", 32, 55, 1, "''", "32''");
+    this.generateSlider("inseam", 32, 55, 1, "''", "32''");
+    this.generateSlider("thigh", 32, 55, 1, "''", "32''");
+    this.generateSlider("hip", 32, 55, 1, "''", "32''");
+    this.generateSlider("shoe_size", 5, 14, 0.5, "", "10");
   },
   
-  generateSlider: function(type, min, max, step, defaultVal) {
+  generateSlider: function(type, min, max, step, suffix, defaultVal) {
     var that = this;
-    var savedVal = this.model.get("height") || 60;
+    var savedVal = this.model.get("+type+") || min;
 
     this.$("#"+type+"-slider").slider({
       min: min,
       max: max,
       step: step,
       value: savedVal,
-      slide: function( event, ui ) {
+      slide: function(event, ui ) {
         var uiVal = ui.value;
-        that.$("#"+type+"_amt").html(uiVal);
+        that.$("#"+type+"_amt").html(uiVal+suffix);
       },
-      change: function( event, ui) {
-        var uiVal = ui.value
+      change: function(event, ui) {
+        var uiVal = ui.value;
         that.$("form input[name='measurements["+type+"]']").val(uiVal);
-        that.$("#"+type+"_amt").html(uiVal);
+        that.$("#"+type+"_amt").html(uiVal+suffix);
       }
     });
     
+    this.$("#"+type+"_min").prepend(min);
+    this.$("#"+type+"_max").prepend(max);
     this.$("#"+type+"_amt").html(defaultVal);
   },
   
@@ -122,11 +143,11 @@ Closet.Views.MeasurementsShow = Backbone.View.extend({
       max: 84,
       step: 1,
       value: savedHeight,
-      slide: function( event, ui ) {
+      slide: function(event, ui ) {
         var uiVal = ui.value;
         that.$("#height_amt").html(toFeet(uiVal));
       },
-      change: function( event, ui) {
+      change: function(event, ui) {
         var uiVal = ui.value
         that.$("form input[name='measurements[height]']").val(uiVal);
         that.$("#height_amt").html(toFeet(uiVal));
