13deffc31e241852880119ca92d7a4bd96e5699a
Grace Do <xgrace@gmail.com>
2014-03-26 18:01:26 -0700

    init on precompile false (heroku)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Closet::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
